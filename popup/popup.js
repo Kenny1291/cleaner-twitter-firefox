831b20d83ed526b6e937f08100d0db25af039a00
Raiquen <raiqueng@live.com>
2023-05-30 17:21:11 +0200

    inital commit

---
+++ popup/popup.js
@@ -0,0 +1,46 @@
+(async () => {
+  const CSSRules = await chrome.storage.sync.get().then(result => {
+    return result.CSSRulesArrayOfObjectsWithNames
+  })
+
+  const h2 = document.querySelector('h2')
+
+  CSSRules.forEach(CSSRule => {
+    const toggleName = CSSRule.name
+      .split('_')
+      .map(word => word[0].toUpperCase() + word.slice(1))
+      .join(' ')
+
+    h2.insertAdjacentHTML(
+      'afterend',
+      `
+      <div class="switch-container">
+        <label for=${CSSRule.name}>${toggleName}</label>
+        <input id=${CSSRule.name} type="checkbox" />
+      </div>
+    `
+    )
+
+    const ruleToggle = document.getElementById(CSSRule.name)
+
+    //set toggles based on storage keys values the first time
+    ruleToggle.checked = CSSRule.active
+
+    ruleToggle.addEventListener('click', function () {
+      toggleStorageKey(CSSRule.name)
+    })
+
+    function toggleStorageKey(CSSRuleName) {
+      chrome.storage.sync.get().then(result => {
+        const CSSRules = result.CSSRulesArrayOfObjectsWithNames
+        const CSSRule = CSSRules.find(rule => rule.name === CSSRuleName)
+        CSSRule.active = !CSSRule.active
+        chrome.storage.sync.set({ CSSRulesArrayOfObjectsWithNames: CSSRules })
+      })
+    }
+  })
+
+  document.getElementById('editCSSRules').addEventListener('click', () => {
+    window.open(chrome.runtime.getURL('options/options.html'))
+  })
+})()
---
+++ utils.js
@@ -0,0 +1,36 @@
+async function getCSSRulesFromStorage() {
+    return await chrome.storage.sync.get().then(result => result.CSSRulesArrayOfObjectsWithNames);
+}
+
+function processCSSRule(rule, CSSRules) {
+    const match = rule.match(/\.([a-z0-9_-]+)/i);
+    const name = match ? match[1] : '';
+    const CSSRule = CSSRules.find(rule => rule.name === name);
+    return { name, rule, active: CSSRule ? CSSRule.active : false }
+}
+
+export async function createCSSRulesArrayOfObjectsWithRuleNames({CSSRulesArray, fetchStateFromStorage = false}) {
+    const CSSRules = fetchStateFromStorage ? await getCSSRulesFromStorage(fetchStateFromStorage) : []
+    return CSSRulesArray.map(rule => processCSSRule(rule, CSSRules))
+}
+
+const defaultCSSRulesArray = [
+  '.hide_tweet_analytics div > a[aria-label$="View Tweet analytics"] {display: none;}',
+  '.hide_twitter_blue a[href="/i/twitter_blue_sign_up"] {display: none;}',
+//   '.hide_explore  a[href="/explore"] {display: none;}',
+//   '.hide_bookmarks a[href="/i/bookmarks"] {display: none;}',
+  '.hide_trends_for_you div > div > section > div[aria-label="Timeline: Trending now"] {display: none;}',
+  '.hide_who_to_follow div > div > aside[aria-label="Who to follow"] {display: none;}',
+  '.hide_footer nav[aria-label="Footer"] {display: none;}',
+  '.hide_messages_drawer div[data-testid="DMDrawer"] {display: none;}',
+//   '.hide_promoted_tweets div:has(> div[arial-label="placement-tracking"] > svg) {display: none;}',
+//   '.hide_verified_organizations a[href="/i/verified-orgs-signup"] {display: none;}',
+//   '.hide_twitter_button a[href="/compose/tweet"] {display: none;}',
+//   '.hide_account_menu div:has(> div > div[aria-label="Account menu"]) {display: none;}',
+//   '.hide_twitter_icon h1 {display: none;}',
+]
+
+export async function setDefaultRules() {
+  const CSSRulesArrayOfObjectsWithNames = await createCSSRulesArrayOfObjectsWithRuleNames({CSSRulesArray: defaultCSSRulesArray})
+  chrome.storage.sync.set({ CSSRulesArrayOfObjectsWithNames })
+}
